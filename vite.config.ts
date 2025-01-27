46e43900b25b90d539fa02793514126f706d9202
Nikolas Guedes da Silva <guedesnikolas@gmail.com>
2025-01-27 19:17:28 -0300

    configurações deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from "vite"
  
 export default defineConfig({
   plugins: [react()],
+  base: "/portifolio_nikolasguedes",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
