83a7a0985b93a98ac466ddecb4907e6629ff8d67
Nikolas Guedes da Silva <guedesnikolas@gmail.com>
2025-01-27 19:26:23 -0300

    test modification deploy

--- tailwind.config.js
+++ tailwind.config.js
@@ -52,7 +52,9 @@ export default {
   			}
   		},
   		animation: {
-  			grid: 'grid 15s linear infinite'
+  			grid: 'grid 15s linear infinite',
+  			marquee: 'marquee var(--duration) infinite linear',
+  			'marquee-vertical': 'marquee-vertical var(--duration) linear infinite'
   		},
   		keyframes: {
   			grid: {
@@ -61,6 +63,22 @@ export default {
   				},
   				'100%': {
   					transform: 'translateY(0)'
+  				}
+  			},
+  			marquee: {
+  				from: {
+  					transform: 'translateX(0)'
+  				},
+  				to: {
+  					transform: 'translateX(calc(-100% - var(--gap)))'
+  				}
+  			},
+  			'marquee-vertical': {
+  				from: {
+  					transform: 'translateY(0)'
+  				},
+  				to: {
+  					transform: 'translateY(calc(-100% - var(--gap)))'
   				}
   			}
   		}
