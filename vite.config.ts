7d9232d873730b7d3813053f612ac1f2091705a0
Nikolas Guedes da Silva <guedesnikolas@gmail.com>
2025-01-27 20:17:12 -0300

    update base path

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from "vite"
  
 export default defineConfig({
   plugins: [react()],
-  base: "/portifolio_nikolasguedes",
+  base: "/portfolio_nikolasguedes",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
