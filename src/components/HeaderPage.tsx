5ef4ccf9e5c1de9fddf7932a67e99b3b97552b7f
Nikolas Guedes da Silva <guedesnikolas@gmail.com>
2025-02-16 19:33:30 -0300

    feat: new bento layout

--- src/components/HeaderPage.tsx
+++ src/components/HeaderPage.tsx
@@ -2,17 +2,17 @@ import Logo_NK from "../img/NK_logo.png";
 
 function HeaderPage() {
   return (
-    <div className="z-50 fixed bg-black border border-white w-[97%] px-20 max-lg:px-8 3xl:container 3xl:mx-auto py-2">
+    <div className="z-50 fixed bg-black w-full border-b-2 border-white px-20 max-lg:px-8 max-h-[8%] min-h-[8%]  3xl:container 3xl:mx-auto py-2 flex justify-center">
       <nav className="flex flex-row items-center w-full justify-between gap-10">
       <img
           src={Logo_NK}
           alt="logo exata"
           className="flex items-center size-min"
         />
-        <ul className="flex flex-row items-center gap-10 max-lg:hidden border border-white">
+        <ul className="flex flex-row items-center gap-10 max-lg:hidden">
           <li>
             <button
-              className="font-normal text-xl tracking-wide relative after:absolute after:bottom-0 after:left-0 after:h-[7px] after:w-full after:origin-bottom-right after:scale-x-0  
+              className="font-thin text-xl tracking-wide relative after:absolute after:bottom-0 after:left-0 after:h-[7px] after:w-full after:origin-bottom-right after:scale-x-0  
       after:bg-white after:transition-transform after:duration-300 after:ease-[cubic-bezier(0.65_0.05_0.36_1)] hover:after:origin-bottom-left hover:after:scale-x-100"
             >
               HOME
@@ -20,7 +20,7 @@ function HeaderPage() {
           </li>
           <li>
             <button
-              className="font-normal text-xl tracking-wide relative after:absolute after:bottom-0 after:left-0 after:h-[7px] after:w-full after:origin-bottom-right after:scale-x-0  
+              className="font-thin text-xl tracking-wide relative after:absolute after:bottom-0 after:left-0 after:h-[7px] after:w-full after:origin-bottom-right after:scale-x-0  
       after:bg-white after:transition-transform after:duration-300 after:ease-[cubic-bezier(0.65_0.05_0.36_1)] hover:after:origin-bottom-left hover:after:scale-x-100"
             >
               SKILLS
@@ -28,7 +28,7 @@ function HeaderPage() {
           </li>
           <li>
             <button
-              className="font-normal text-xl tracking-wide relative after:absolute after:bottom-0 after:left-0 after:h-[7px] after:w-full after:origin-bottom-right after:scale-x-0  
+              className="font-thin text-xl tracking-wide relative after:absolute after:bottom-0 after:left-0 after:h-[7px] after:w-full after:origin-bottom-right after:scale-x-0  
       after:bg-white after:transition-transform after:duration-300 after:ease-[cubic-bezier(0.65_0.05_0.36_1)] hover:after:origin-bottom-left hover:after:scale-x-100"
             >
               PROJECTS
@@ -36,7 +36,7 @@ function HeaderPage() {
           </li>
           <li>
             <button
-              className="font-normal text-xl tracking-wide relative after:absolute after:bottom-0 after:left-0 after:h-[7px] after:w-full after:origin-bottom-right after:scale-x-0 
+              className="font-thin text-xl tracking-wide relative after:absolute after:bottom-0 after:left-0 after:h-[7px] after:w-full after:origin-bottom-right after:scale-x-0 
       after:bg-white after:transition-transform after:duration-300 after:ease-[cubic-bezier(0.65_0.05_0.36_1)] hover:after:origin-bottom-left hover:after:scale-x-100"
             >
               CONTACT
--- src/components/MainSection.tsx
+++ src/components/MainSection.tsx
@@ -6,9 +6,6 @@ import Instagran_Icon from "../img/instagram_icon.png";
 import { Marquee } from "@/components/ui/marquee";
 import Text_Marquee_Outline from "../img/text_marque_outline.png";
 import Text_Marquee from "../img/text_marque.png";
-import Tunel_Grid from "../img/tunel_grid.png";
-import Green_Dot from "../img/green_dot.png";
-import { Player, Controls } from "@lottiefiles/react-lottie-player";
 
 import { ReactNode } from "react";
 
@@ -30,83 +27,53 @@ function MainSection() {
   const firstRow = countries.slice(0, countries.length / 1);
 
   return (
-    <section className="bg-[var(--Cinzabase)] w-full h-[100vh]">
-      <div className="fixed z-50 right-0 top-1/2 transform bg-black border border-white -translate-y-1/2 flex flex-col items-center justify-center w-[4%] h-full gap-y-20">
-        <img
-          src={Github_Icon}
-          alt="Github Icon"
-          className="flex items-center size-min cursor-pointer"
-        />
-        <img
-          src={Linkdin_Icon}
-          alt="LinkedIn Icon"
-          className="flex items-center size-min cursor-pointer"
-        />
-        <img
-          src={Instagran_Icon}
-          alt="Instagram Icon"
-          className="flex items-center size-min cursor-pointer"
-        />
-      </div>
-
-      <div className="flex flex-col items-center justify-center w-full h-[80%]">
-        <div className="flex flex-row items-center justify-start w-full h-full">
-          <div className="flex flex-col items-center justify-start h-full w-[40%]  border border-white ">
-            <div className="bg-white w-[100%] h-[15%] mt-[4.4rem] flex items-center px-10 justify-between">
-              <div className="flex items-center gap-x-2">
-                <img
-                  src={Green_Dot}
-                  alt="green dot"
-                  className=" size-5 object-contain"
-                />
-                <p className="text-black font-bold text-3xl">MY CHAT</p>
-              </div>
+    <section className="bg-[var(--Cinzabase)] w-full h-[100vh] pt-16 px-20">
+      <div className="flex flex-row items-center justify-center w-full h-full gap-6 py-10">
+        <div className="flex flex-col items-center justify-start h-full w-[40%] gap-6">
+          <div className="flex flex-row w-full h-[70%] gap-6">
+            <div className="flex flex-col w-[80%] h-full border-2 border-white rounded-xl">
+              <div className="w-full h-[5em] bg-white relative rounded-t-lg flex flex-col items-end justify-center px-5 gap-y-2">
               <img
                 src={Foto_Perfil}
-                alt="Foto de Perfil"
-                className=" size-14 items-center object-contain"
+                alt="Foto Perfil"
+                className="flex items-center absolute top-10 left-10 size-40 cursor-pointer"
+              />
+                <p className="text-black">Nikolas Guedes</p>
+                <p className="text-black">Front end Developer</p>
+              </div>
+              
+            </div>
+            <div className="flex flex-col w-[20%] h-full border-2 items-center justify-center gap-y-20 border-white rounded-xl">
+              <img
+                src={Github_Icon}
+                alt="Github Icon"
+                className="flex items-center size-10 cursor-pointer"
+              />
+              <img
+                src={Linkdin_Icon}
+                alt="LinkedIn Icon"
+                className="flex items-center size-10 cursor-pointer"
+              />
+              <img
+                src={Instagran_Icon}
+                alt="Instagram Icon"
+                className="flex items-center size-10 cursor-pointer"
               />
             </div>
-            <div className="w-[100%] h-full flex flex-col items-start px-10 justify-between">
-              <div className="w-[100%] h-full flex flex-col gap-y-5 mt-5">
-                <div className="flex flex-row items-center justify-start">
-                  <p className="font-light text-lg border p-2 border-white rounded-xl">
-                    Hi everyone!
-                  </p>
-                </div>
-                <div className="flex flex-row items-center justify-start">
-                  <p className="font-light text-lg border p-2 border-white rounded-xl">
-                    my name is{" "}
-                    <span className="text-transparent bg-clip-text bg-gradient-to-r from-[var(--CorPrimaria)] to-[var(--CorSecundaria)] font-black text-xl">
-                      Nikolas Guedes
-                    </span>{" "}
-                    and this is my portifolio.
-                  </p>
-                </div>
-                <div className="flex flex-row items-center justify-start">
-                  <p className="font-light text-lg border p-2 border-white rounded-xl">
-                    Hope you liked =)
-                  </p>
-                </div>
-                {/* <div className="flex flex-row items-center justify-end">
-                  <p className="ffont-light text-lg border p-2 border-white rounded-xl">
-                    Lets talk!
-                  </p>
-                </div>
-                <div className="flex flex-row items-center justify-start">
-                  <p className="font-light text-lg border p-2 border-white rounded-xl">
-                   Awsome!
-                  </p>
-                </div>  */}
-              </div>
-              <div className="flex flex-row items-center justify-center my-5 w-full">
-                <p className="font-black text-lg border px-8 py-3 mb-2 text-black bg-white rounded-xl cursor-pointer">
-                  CONTACT ME
-                </p>
-              </div>
-            </div>
           </div>
-          <div className="flex flex-row items-center justify-center h-full w-[60%]">
+          <div className="flex flex-col w-full h-[30%] border-2 items-start justify-start p-5 border-white rounded-xl">
+            <h2 className=" bg-white rounded-lg px-10 py-3 text-black mb-4">
+              About me
+            </h2>
+            <p className="text-justify leading-loose">
+              I've worked as a game developer and UI Designer, currently focused
+              on improving my skills related to programming and creating
+              captivating and intuitive interfaces.
+            </p>
+          </div>
+        </div>
+        <div className="flex flex-row items-center justify-center h-full w-[60%]">
+          <div className="flex w-full h-full">
             <div className="relative w-full h-full">
               <ShaderGradientCanvas
                 style={{
@@ -121,8 +88,8 @@ function MainSection() {
               </ShaderGradientCanvas>
 
               <div className="z-10 absolute w-full h-full">
-                <div className="bg-transparent border-white border w-full h-full">
-                  <div className="flex flex-col justify-end w-full h-full gap-y-10">
+                <div className="bg-transparent border-white border-2 rounded-xl w-full h-full">
+                  <div className="flex flex-col justify-center w-full h-full gap-y-10">
                     <Marquee className="[--duration:60s]">
                       {firstRow.map(() => (
                         <ReviewCard
@@ -233,75 +200,6 @@ function MainSection() {
                     </Marquee>
                   </div>
                 </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="flex flex-row h-[20%] items-start justify-start w-full border border-white">
-        <div className="w-[20%] h-full">
-          <div className="relative w-full h-full border-r-2 border-white">
-            <ShaderGradientCanvas
-              style={{
-                position: "absolute",
-                top: 0,
-              }}
-            >
-              <ShaderGradient
-                control="query"
-                urlString="https://www.shadergradient.co/customize?animate=on&axesHelper=off&bgColor1=%23000000&bgColor2=%23000000&brightness=0.8&cAzimuthAngle=270&cDistance=0.5&cPolarAngle=180&cameraZoom=14.6&color1=%236A00FF&color2=%231D00FF&color3=%23FF5ECC&destination=onCanvas&embedMode=off&envPreset=city&format=gif&fov=80&frameRate=10&gizmoHelper=hide&grain=on&lightType=env&pixelDensity=1&positionX=-0.1&positionY=0&positionZ=0&range=enabled&rangeEnd=40&rangeStart=0&reflection=0.4&rotationX=0&rotationY=130&rotationZ=70&shader=defaults&type=sphere&uAmplitude=3.2&uDensity=0.8&uFrequency=5.5&uSpeed=0.3&uStrength=0.3&uTime=0&wireframe=false&zoomOut=true"
-              />
-            </ShaderGradientCanvas>
-
-            <div className="z-10 absolute w-full h-full"></div>
-            <div className="-z-40 absolute flex justify-center w-full h-full">
-              <img
-                src={Tunel_Grid}
-                alt="tunel grid"
-                className="size-full cursor-pointer"
-              />
-            </div>
-          </div>
-        </div>
-        <div className="w-[20%] h-full flex flex-col justify-between items-start gap-x-5 p-3">
-          <div className="flex w-full h-full p-2">
-            <p>
-              STATUS: <span className="pl-1 font-black">STEADY</span>
-            </p>
-          </div>
-          <div className="flex flex-row items-start justify-start w-full h-full">
-            <Player
-              autoplay
-              src="https://lottie.host/a7a1fb1f-8890-4c4f-bd89-d998690df07d/TM357qwJHu.json"
-              loop
-              style={{ width: "5em", height: "100%" }}
-            >
-              <Controls
-                visible={false}
-                buttons={["play", "repeat", "frame", "debug"]}
-              />
-            </Player>
-            <Player
-              autoplay
-              src="https://lottie.host/a7a1fb1f-8890-4c4f-bd89-d998690df07d/TM357qwJHu.json"
-              loop
-              style={{ width: "5em", height: "100%" }}
-            >
-              <Controls
-                visible={false}
-                buttons={["play", "repeat", "frame", "debug"]}
-              />
-            </Player>
-          </div>
-        </div>
-        <div className="w-full h-full">
-          <div className="flex flex-col items-end justify-center h-full pr-22 text-[var(--CinzaEscuro)] text-xs p-3">
-            <div className="flex w-72 flex-col items-start justify-end gap-y-3">
-              <p>TERMINAL</p>
-              <div className="flex w-72 flex-col items-start justify-end gap-y-3">
-                <p>Loading stuff_</p>
-                <p>Probably broke something</p>
-                <p>checking for bugs_</p>
               </div>
             </div>
           </div>
